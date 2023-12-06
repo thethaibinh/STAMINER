2f782c50f1e7428e84bd19df695125fb903c9565
Binh Nguyen <thethaibinh@gmail.com>
2023-12-07 08:16:30 +1100

    Planner: check goal distance horizontally, Ox 1m closer; Flightmare: Change to ETH forest; setup: add open3D python

--- setup_py.bash
+++ setup_py.bash
@@ -9,19 +9,19 @@ fi
 project_path=$PWD
 echo $project_path
 
-# 
+#
 echo "export FLIGHTMARE_PATH=$project_path/flightmare" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python3-pip
-# 
+#
 echo "Createing an conda environment from the environment.yaml file. Make sure you have anaconda installed"
 conda env create -f environment.yaml
 
-# 
+#
 echo "Source the anaconda environment. If errors, change to the right anaconda path."
 source ~/anaconda3/etc/profile.d/conda.sh
 
-# 
+#
 echo "Actiavte the environment"
 conda activate agileflight
 
@@ -36,9 +36,9 @@ echo "Install RPG baseline"
 cd $project_path/flightmare/flightpy/flightrl
 pip install .
 
-pip install opencv-contrib-python flightgym rpg_baselines
+pip install opencv-contrib-python flightgym rpg_baselines open3d
 # echo "Run the first vision demo."
-# cd $project_path/envtest 
+# cd $project_path/envtest
 # python3 -m python.run_vision_demo --render 1
 
 echo "Done!"
